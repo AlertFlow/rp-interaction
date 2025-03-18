b7ed31173b8074f459864affdb695b9921b23d4b
Justin Neubert <neubert_justin@outlook.com>
2025-03-18 12:10:21 +0100

    update go version to 1.24.0; modify import paths and update dependencies; refactor ExecuteTask and Info methods

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/v1Flows/runner/pkg/executions"
 	"github.com/v1Flows/runner/pkg/plugins"
 
-	"github.com/v1Flows/alertFlow/services/backend/pkg/models"
+	"github.com/v1Flows/shared-library/pkg/models"
 
 	"github.com/hashicorp/go-plugin"
 )
@@ -27,28 +27,33 @@ func (p *Plugin) ExecuteTask(request plugins.ExecuteTaskRequest) (plugins.Respon
 
 	err := executions.UpdateStep(request.Config, request.Execution.ID.String(), models.ExecutionSteps{
 		ID: request.Step.ID,
-		Messages: []string{
-			`Waiting for user interaction`,
-			`Timeout: ` + strconv.Itoa(timeout) + ` seconds`,
+		Messages: []models.Message{
+			{
+				Title: "Interaction",
+				Lines: []string{
+					`Waiting for user interaction`,
+					`Timeout: ` + strconv.Itoa(timeout) + ` seconds`,
+				},
+			},
 		},
 		Interactive: true,
 		Status:      "interactionWaiting",
 		StartedAt:   time.Now(),
-	})
+	}, request.Platform)
 	if err != nil {
 		return plugins.Response{
 			Success: false,
 		}, err
 	}
 
-	executions.SetToInteractionRequired(request.Config, request.Execution)
+	executions.SetToInteractionRequired(request.Config, request.Execution, request.Platform)
 
 	var stepData models.ExecutionSteps
 
 	// pull current action status from backend every 10 seconds
 	startTime := time.Now()
 	for {
-		stepData, err = executions.GetStep(request.Config, request.Execution.ID.String(), request.Step.ID.String())
+		stepData, err = executions.GetStep(request.Config, request.Execution.ID.String(), request.Step.ID.String(), request.Platform)
 
 		if stepData.Interacted {
 			break
@@ -65,16 +70,21 @@ func (p *Plugin) ExecuteTask(request plugins.ExecuteTaskRequest) (plugins.Respon
 		if timeout > 0 && time.Since(startTime).Seconds() >= float64(timeout) {
 			err = executions.UpdateStep(request.Config, request.Execution.ID.String(), models.ExecutionSteps{
 				ID: request.Step.ID,
-				Messages: []string{
-					"Interaction timed out",
-					"Automatically approved & continuing to the next step",
+				Messages: []models.Message{
+					{
+						Title: "Interaction",
+						Lines: []string{
+							"Interaction timed out",
+							"Automatically approved & continuing to the next step",
+						},
+					},
 				},
 				Status:              "success",
 				FinishedAt:          time.Now(),
 				Interacted:          true,
 				InteractionApproved: true,
 				InteractionRejected: false,
-			})
+			}, request.Platform)
 			if err != nil {
 				return plugins.Response{
 					Success: false,
@@ -87,21 +97,26 @@ func (p *Plugin) ExecuteTask(request plugins.ExecuteTaskRequest) (plugins.Respon
 		}
 	}
 
-	executions.SetToRunning(request.Config, request.Execution)
+	executions.SetToRunning(request.Config, request.Execution, request.Platform)
 
 	if stepData.InteractionRejected {
 		err = executions.UpdateStep(request.Config, request.Execution.ID.String(), models.ExecutionSteps{
 			ID: request.Step.ID,
-			Messages: []string{
-				"Interaction rejected",
-				"Execution canceled",
+			Messages: []models.Message{
+				{
+					Title: "Interaction",
+					Lines: []string{
+						"Interaction rejected",
+						"Execution canceled",
+					},
+				},
 			},
 			Status:              "canceled",
 			FinishedAt:          time.Now(),
 			Interacted:          true,
 			InteractionRejected: true,
 			InteractionApproved: false,
-		})
+		}, request.Platform)
 		if err != nil {
 			return plugins.Response{
 				Success: false,
@@ -115,14 +130,19 @@ func (p *Plugin) ExecuteTask(request plugins.ExecuteTaskRequest) (plugins.Respon
 		}, nil
 	} else if stepData.InteractionApproved {
 		err = executions.UpdateStep(request.Config, request.Execution.ID.String(), models.ExecutionSteps{
-			ID:                  request.Step.ID,
-			Messages:            []string{"Interaction approved"},
+			ID: request.Step.ID,
+			Messages: []models.Message{
+				{
+					Title: "Interaction",
+					Lines: []string{"Interaction approved"},
+				},
+			},
 			Status:              "success",
 			FinishedAt:          time.Now(),
 			Interacted:          true,
 			InteractionRejected: false,
 			InteractionApproved: true,
-		})
+		}, request.Platform)
 		if err != nil {
 			return plugins.Response{
 				Success: false,
@@ -138,19 +158,19 @@ func (p *Plugin) ExecuteTask(request plugins.ExecuteTaskRequest) (plugins.Respon
 	}, nil
 }
 
-func (p *Plugin) HandleAlert(request plugins.AlertHandlerRequest) (plugins.Response, error) {
+func (p *Plugin) EndpointRequest(request plugins.EndpointRequest) (plugins.Response, error) {
 	return plugins.Response{
 		Success: false,
 	}, errors.New("not implemented")
 }
 
-func (p *Plugin) Info() (models.Plugins, error) {
-	var plugin = models.Plugins{
+func (p *Plugin) Info() (models.Plugin, error) {
+	var plugin = models.Plugin{
 		Name:    "Interaction",
 		Type:    "action",
-		Version: "1.1.2",
+		Version: "1.2.0",
 		Author:  "JustNZ",
-		Actions: models.Actions{
+		Action: models.Action{
 			Name:        "Interaction",
 			Description: "Wait for user interaction to continue",
 			Plugin:      "interaction",
@@ -166,7 +186,7 @@ func (p *Plugin) Info() (models.Plugins, error) {
 				},
 			},
 		},
-		Endpoints: models.AlertEndpoints{},
+		Endpoint: models.Endpoint{},
 	}
 
 	return plugin, nil
@@ -183,13 +203,13 @@ func (s *PluginRPCServer) ExecuteTask(request plugins.ExecuteTaskRequest, resp *
 	return err
 }
 
-func (s *PluginRPCServer) HandleAlert(request plugins.AlertHandlerRequest, resp *plugins.Response) error {
-	result, err := s.Impl.HandleAlert(request)
+func (s *PluginRPCServer) EndpointRequest(request plugins.EndpointRequest, resp *plugins.Response) error {
+	result, err := s.Impl.EndpointRequest(request)
 	*resp = result
 	return err
 }
 
-func (s *PluginRPCServer) Info(args interface{}, resp *models.Plugins) error {
+func (s *PluginRPCServer) Info(args interface{}, resp *models.Plugin) error {
 	result, err := s.Impl.Info()
 	*resp = result
 	return err
