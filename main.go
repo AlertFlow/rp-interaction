523733e17530bd9be49ebdd9ab93c0f10b8933f4
Justin Neubert <neubert_justin@outlook.com>
2025-03-22 11:13:26 +0100

    update go.mod and go.sum dependencies; bump runner to v1.0.1 and shared-library to v1.0.6; update Info method signature and version to 1.2.1

--- main.go
+++ main.go
@@ -164,11 +164,11 @@ func (p *Plugin) EndpointRequest(request plugins.EndpointRequest) (plugins.Respo
 	}, errors.New("not implemented")
 }
 
-func (p *Plugin) Info() (models.Plugin, error) {
+func (p *Plugin) Info(request plugins.InfoRequest) (models.Plugin, error) {
 	var plugin = models.Plugin{
 		Name:    "Interaction",
 		Type:    "action",
-		Version: "1.2.0",
+		Version: "1.2.1",
 		Author:  "JustNZ",
 		Action: models.Action{
 			Name:        "Interaction",
@@ -209,8 +209,8 @@ func (s *PluginRPCServer) EndpointRequest(request plugins.EndpointRequest, resp 
 	return err
 }
 
-func (s *PluginRPCServer) Info(args interface{}, resp *models.Plugin) error {
-	result, err := s.Impl.Info()
+func (s *PluginRPCServer) Info(request plugins.InfoRequest, resp *models.Plugin) error {
+	result, err := s.Impl.Info(request)
 	*resp = result
 	return err
 }
